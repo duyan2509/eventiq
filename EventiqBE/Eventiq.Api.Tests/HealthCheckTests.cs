7961605a6f808c7f9699641d55e409d2bb847e21
duyan2509 <22520020@gm.uit.edu.vn>
2025-09-10 08:58:55 +0700

    add test project

---
+++ EventiqBE/Eventiq.Api.Tests/HealthCheckTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Eventiq.Api.Tests;
+
+public class HealthCheckTests
+{
+    [Fact]
+    public async Task Api_Should_Return_Healthy()
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:5000/health");
+        Assert.True(response.IsSuccessStatusCode);
+    }
+}
