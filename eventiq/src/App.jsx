5d779b620964e7281d5d9516edc0ef60134f87fc
duyan2509 <22520020@gm.uit.edu.vn>
2025-09-11 20:12:13 +0700

    initial frontend

---
+++ eventiq/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+        <h1 className="text-5xl font-bold">🏠 Home Page</h1>
+        <p className="mt-4 text-lg">TailwindCSS hoạt động rồi 🎉</p>
+      </div>
+    </>
+  )
+}
+
+export default App
